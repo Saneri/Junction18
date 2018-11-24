b4b9247ef883bb694b0b57ab11e64101a27dc4cc
Petteri Pulkkinen <petteri.pulkkinen@aalto.fi>
2018-11-24 15:45:54 +0200

    Temporary version
    Signed-off-by: Petteri Pulkkinen <petteri.pulkkinen@aalto.fi>

--- src/data2spatial.py
+++ src/data2spatial.py
@@ -1,4 +1,14 @@
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+class MesasureData(object):
+    def __init__(self, data):
+        self.acc = data
+        self.length = data.shape[0]
+        self.ang_vel = None
+        self.dt = 0.1
 
 
 class Derivative(object):
@@ -16,28 +26,30 @@ class SpatialConverter(object):
     def __init__(self, m_data):
         '''Initialize class'''
         self.m_data = m_data  # measured data (acceleration and angular velocity)
-        self.pos = np.zeros(m_data.length, 3)
-        self.rotation = np.eye(3)
-        self.velocity = np.zeros(m_data.length, 3)
-        self.ang = np.zeros(m_data.length, 3)
+        self.m_data.acc[:] = self.m_data.acc[:] - np.mean(self.m_data.acc[0:50, :], axis=0)
 
-    def _evaluate(self, dt, d):
-        dpos = self.velocity[self.ind] + dt * d.dv
-        dv = self.acceleration[self.ind]
+        self.pos = np.zeros((m_data.length, 3))
+        self.rotation = np.eye(3)
+        self.velocity = np.zeros((m_data.length, 3))
+        self.ang = np.zeros((m_data.length, 3))
+
+    def _evaluate(self, vel, acc, dt, d):
+        dpos = vel + dt * d.dv
+        dv = acc
         return Derivative(dpos, dv)
 
     def take_step(self, acceleration, velocity, pos, dt):
         der = Derivative(velocity, acceleration)
-        a = self._evaluate(0, der)
-        b = self._evaluate(0.5*dt, a)
-        c = self._evaluate(0.5*dt, b)
-        d = self._evaluate(dt, c)
+        a = self._evaluate(velocity, acceleration, 0, der)
+        b = self._evaluate(velocity, acceleration, 0.5*dt, a)
+        c = self._evaluate(velocity, acceleration, 0.5*dt, b)
+        d = self._evaluate(velocity, acceleration, dt, c)
 
         dxdt = 1/6 * (a.dpos + 2 * (b.dpos + c.dpos) + d.dpos)
         dvdt = 1/6 * (a.dv + 2 * (b.dv + c.dv) + d.dv)
 
-        position = pos + dxdt * self.dt
-        velocity = velocity + dvdt * self.dt
+        position = pos + dxdt * self.m_data.dt
+        velocity = velocity + dvdt * self.m_data.dt
 
         return position, velocity
 
@@ -65,16 +77,46 @@ class SpatialConverter(object):
         z = self.angle[index, 0]
         self.rotation = self._x_r(x).dot(self._y_r(y)).dot(self._z_r(z))
 
-    def update_postition(self, pos, velocity, index):
-        R_inv = np.linalg.inv(self.rotation)
-        self.pos[index] = R_inv.dot(pos)
-        self.velocity[index] = R_inv.dot(velocity)
+    def update_position(self, pos, velocity, index):
+        # R_inv = np.linalg.inv(self.rotation)
+        # self.pos[index] = R_inv.dot(pos)
+        # self.velocity[index] = R_inv.dot(velocity)
+        self.pos[index] = pos
+        self.velocity[index] = velocity
 
     def convert_to_spatial_coordinates(self):
         for i in range(self.m_data.length-1):
-            acc = self.m_data.acc[i, :]
-            ang_vel = self.m_data.ang_vel[i, :]
-            dt = self.m_data.timestamps[i+1] - self.m_data.timestamps[i]
-            pos, vel = self.take_step(acc, self.velocity, self.pos, dt)
-            self.update_rotation(ang_vel, i)
-            self.update_postition(pos, vel, i)
+            # ang_vel = self.m_data.ang_vel[i]
+            # dt = self.m_data.timestamps[i + 1] - self.m_data.timestamps[i]
+            dt = self.m_data.dt
+            pos, vel = self.take_step(
+                self.m_data.acc[i], self.velocity[i], self.pos[i], dt)
+            # self.update_rotation(ang_vel, i)
+            self.update_position(pos, vel, i+1)
+
+
+if __name__ == '__main__':
+    # df = pd.read_csv('acceleration.csv')
+    # data = df.values[:, 0:3]
+    # m_data = MesasureData(data)
+    # c = SpatialConverter(m_data)
+    # c.convert_to_spatial_coordinates()
+    # print(c.pos)
+    # fig, (ax1, ax2) = plt.subplots(2, 1)
+    # ax1.plot(c.pos)
+    # ax2.plot(m_data.acc)
+    # plt.legend(['x', 'y', 'z'])
+    # import pdb
+    # pdb.set_trace()
+    # plt.show()
+    # # c = SpatialConverter()
+
+    Ax = [-np.cos(x) for x in range(100)]
+    Ay = [-np.sin(x) for x in range(100)]
+    Az = [0 for x in range(100)]
+    A = np.array([Ax, Ay, Az])
+    m_data = MesasureData(A)
+
+    c = SpatialConverter(m_data)
+    c.convert_to_spatial_coordinates()
+    plt.plot(c.pos)
