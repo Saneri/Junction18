0a18d4baf808f1b57295608eb7bf631341bee993
Saneri <santeri.suitiala@aalto.fi>
2018-11-24 14:47:18 +0200

    Give matrix structure to csv2data file output
    Signed-off-by: Saneri <santeri.suitiala@aalto.fi>

--- src/csv2data.py
+++ src/csv2data.py
@@ -1,39 +1,45 @@
-
 import csv
+import numpy as np
+
 
 class DataReader:
-    
+
     def __init__(self,csvfile):
         self.file = csvfile
-        self.time = []
-        self.x = []
-        self.y = []
-        self.z = []
-        self.length = None
-    
+        #self.acceleration =  np.zeros(0, 3)
+        #self.velocity = np.zeros(0, 3)
 
-    def compile_lists(self,row,lista,column):
-        row = ''.join(row)
-        tiedot = row.split(",")
-        lista.append(tiedot[column])
+#        self.time = []
+#        self.x = []
+#        self.y = []
+#        self.z = []
+#        self.length = None
+
+#    def compile_lists(self,row,lista,column):
+#        row = ''.join(row)
+#        tiedot = row.split(",")
+#        lista.append(tiedot[column])
 
     def read_file(self):
-    
-        timestampcolumn = 0
+
+        servicecolumn = 0
         xcolumn=1
         ycolumn=2
         zcolumn=3
-        
+
         with open(self.file, 'r') as csvfile:
-            spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
-            for row in spamreader:
-                DataReader.compile_lists(row,self.time,timestampcolumn)
-                DataReader.compile_lists(row,self.x,xcolumn)
-                DataReader.compile_lists(row,self.y,ycolumn)
-                DataReader.compile_lists(row,self.z,zcolumn)
-            self.length = len(self.time)
-            
-        
-    
-        
-        
+            reader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+            lastServiceValue = None
+            for row in reader:
+                row = ''.join(row)
+                row = row.split(",")
+                print(str(row[0]) + "  :  " + str(lastServiceValue))
+                if (row[0] == "LinearAcc") and  (lastServiceValue == "AngularVelocity"):
+                    columnToArray(row, row[0])
+                elif (row[0] == "AngularVelocity") and (lastServiceValue == "LinearAcc"):
+                    columnToArray(row, row[0])
+                lastServiceValue = row[0]
+
+    def columnToArray(self, row, type):
+        pass
+        # Ville hoitaa
