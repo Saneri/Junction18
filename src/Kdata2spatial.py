a54ef41c73b699dca54e9c66e6d733163b49c685
Kristian Arjas <kristian.arjas95@gmail.com>
2018-11-24 22:18:10 +0200

    maybe now?

--- src/Kdata2spatial.py
+++ src/Kdata2spatial.py
@@ -30,7 +30,7 @@ class state():
 
 
 def eulerStep(state, step):
-
+    
     R = state.getPos()
     V = state.getVel()
     F = state.getF()
@@ -52,28 +52,34 @@ def smoothen(vec, win = 9):
         for j in range(int(-win/2), int(win/2)):
             if j != 0:
                 avg += s*vec[i + j]
-        vec2[i] = avg
+        vec2[i] = avg    
         #vec2[i] = 0.2*vec[i-2] + 0.3*vec[i-1] + 0.3*vec[i + 1] + 0.2*vec[i +2]
 
     return vec2
 
 #if __name__=='__main__':
 
-def getSpatial(fname = 'src/Mallisuoritus.csv'):
+def getSpatial(datareader, fname = 'src/Mallisuoritus.csv'):
+    
+   # df = pd.read_csv(fname)
+   # data = df.values[:, 0:3]
 
-    df = pd.read_csv(fname)
-    data = df.values[:, 0:3]
-    data -= data[0, :]
+
+    data = np.asarray(datareader.acceleration)
+    data -= data[0,:]
+    
+
+    #data -= data[0, :]
     XX = data[:,0]
     YY = data[:,1].dot(-1)
     ZZ = data[:,2]
-    #XX = smoothen(XX)
+    #XX = smoothen(XX) 
     #YY = smoothen(YY)
     #ZZ = smoothen(ZZ)
     #XX = smoothen(XX)
-    #YY = smoothen(YY)
+    #YY = smoothen(YY)    
     #ZZ = smoothen(ZZ)
-
+    
     data_smooth = np.transpose(np.array([XX,YY,ZZ]))
     r = np.array([0,0,0])
     v = np.array([0,0,0])
@@ -82,11 +88,11 @@ def getSpatial(fname = 'src/Mallisuoritus.csv'):
 
     R = []
     V = []
-
+    
     for i in range(0,data.shape[0]):
         R.append(S.pos)
         V.append(S.vel)
-        S.takeStep("euler", 1)
+        S.takeStep("euler", 1) 
 
     R = np.asarray(R)
     V = np.asarray(V)
@@ -99,21 +105,22 @@ def getSpatial(fname = 'src/Mallisuoritus.csv'):
     out['S1'] = R.dot(0.1)
 
     #return out
-
+    
     print(data.shape)
-
+    
     fig = plt.figure()
     ax1 = fig.add_subplot(311)
     ax2 = fig.add_subplot(312)
     ax3 = fig.add_subplot(313)
-
+    
     ax1.plot(R)
     ax2.plot(V)
     ax3.plot(data - data[0,:])
 
-
+    
     plt.show()
-
+   
+    
     return out
 
 if __name__ == '__main__':
