41311488b628bb49170e131311b963f2dce23d43
Wildu55 <ville.nevasuo@aalto.fi>
2018-11-24 21:51:09 +0200

    add ProjectionViewer functionality

--- src/main.py
+++ src/main.py
@@ -35,23 +35,40 @@ def fetcher(answer):
     # Creates an instance of csvFetcher to fetch latest csv-files
     print("Test:" +str(answer))
     fetcher = csvFetcher()
-    if answer == "t":
-        is_model = False
-    elif answer == "m":
-        is_model = True
-    else:
-        raise Exception("ERROR")
-
-    filepath = fetcher.fetch(is_model)
-    reader = DataReader(filepath)
+    answer = input("Do you want to create a new model? (y = yes, n=no)\n")
+    if answer == "yes" or answer == "y":
+        input("Please perform a model measurement with your sensor and then press any key to continue\n")
+        modelpath = fetcher.fetch(True)
+    input("Please perform a test measurement with your sensor and then press any key to continue\n")
+    testpath = fetcher.fetch(False)
+    reader = DataReader(testpath)
     reader.read_file()
+    return modelpath,testpath
+
+
+def view_projection(modelpath, testpath):
+
+    pv = ProjectionViewer(1600, 1200,modelpath,testpath)
+
+    arm = wireframe.Wireframe()
+    arm.addNodes([(0, 0, 0), (100, 0, 0)])
+    arm.addEdges([(0, 1)])
+
+    arm2 = wireframe.Wireframe()
+    arm2.addNodes([(0,0,0), (100,00,0)])
+    arm2.addEdges([(0,1)])
+
+    pv.addWireframe('model', arm2)
+    pv.addWireframe('arm', arm)
+    pv.run()
 
 
 def main():
 
     set_paths()
     answer = ask_data_type()
-    fetcher(answer)
+    modelpath, testpath = fetcher(answer)
+    view_projection(modelpath, testpath)
 
 
 main()
